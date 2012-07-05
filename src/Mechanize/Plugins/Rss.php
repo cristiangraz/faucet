7e742a9beaafd999b4f57c5acbf065baf45bb63e
cristiangraz <cristian.graziano@gmail.com>
2012-07-05 08:46:25 -0700

    Adding login for the Rss plugin to consume a feed

--- src/Mechanize/Plugins/Rss.php
+++ src/Mechanize/Plugins/Rss.php
@@ -2,8 +2,20 @@
 
 namespace Mechanize\Plugins;
 
+use Zend\Feed\Reader\Reader;
+
 class Rss extends AbstractPlugin implements PluginInterface
 {
+	/**
+	 * Reads the rss feed
+	 *
+	 * @return Zend\Feed\Reader\Feed\FeedInterface
+	 */
+	public function getFeed()
+	{
+		return Reader::importString($this->getParser()->getBody());
+	}
+
 	/**
 	 * Define the plugin's alias
 	 *
