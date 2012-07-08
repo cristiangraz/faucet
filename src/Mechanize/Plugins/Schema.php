da269016f0337f2092ee80175379f74cef9b97d9
cristiangraz <cristian.graziano@gmail.com>
2012-07-07 22:40:07 -0700

    Initial schema.org plugin - needs lots of work still

--- src/Mechanize/Plugins/Schema.php
+++ src/Mechanize/Plugins/Schema.php
@@ -4,6 +4,37 @@ namespace Mechanize\Plugins;
 
 class Schema extends AbstractPlugin implements PluginInterface
 {
+	public function getSchemas()
+	{
+		$schemas = $this->find('//*[@itemscope]');
+
+		$elements = array();
+		foreach ($schemas as $i => $schema) {
+			// Find Properties
+			$elements[$i]['type'] = $schema->itemtype;
+
+			// Find more elements
+			$properties = $schema->find('//*[@itemprop]');
+
+			if ($properties->length === 0) {
+				continue;
+			}
+
+			foreach ($properties as $property) {
+				if ($property->hasAttribute('content')) {
+					$elements[$i][$property->itemprop] = $property->content;
+				} else {
+					$elements[$i][$property->itemprop] = trim($property->getText());
+				}
+			}
+		}
+
+		echo '<pre>';
+		print_r($elements);
+
+		exit;
+	}
+
 	/**
 	 * Define the plugin's alias
 	 *
