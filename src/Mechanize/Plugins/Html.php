fbd17f98d435c4b2f91397a410004a9636daf098
cristiangraz <cristian.graziano@gmail.com>
2012-07-07 22:11:34 -0700

    Moving canonical URL logic into the HTML plugin

--- src/Mechanize/Plugins/Html.php
+++ src/Mechanize/Plugins/Html.php
@@ -6,6 +6,8 @@ use Zend\Validator\Regex;
 
 class Html extends AbstractPlugin implements PluginInterface
 {
+    protected $canonicalUrl = null;
+
     /**
      * Returns the page title of the current page
      *
@@ -124,6 +126,43 @@ class Html extends AbstractPlugin implements PluginInterface
 
         return $favicon->href;
     }
+
+    /**
+     * Determines if the url has a canonical url
+     *
+     * @return mixed The canonical url or bool false
+     */
+    public function hasCanonicalUrl()
+    {
+        if (!is_null($this->canonicalUrl)) {
+            return $this->canonicalUrl !== null;
+        }
+
+        $canonical = $this->findOne('/html/head/link[@rel="canonical"]');
+
+        if ($canonical->length === 1) {
+            $this->canonicalUrl = $this->getParser()->getAbsoluteUrl($canonical->href);
+
+            return $this->hasCanonicalUrl();
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns the absolute canonical url to the page
+     *
+     * @return mixed The canonical url or bool false
+     */
+    public function getCanonicalUrl()
+    {
+        if ($this->hasCanonicalUrl()) {
+            return $this->canonicalUrl;
+        }
+
+        return false;
+    }
+    
 
     /**
      * Retrieve page rss feeds
--- src/Mechanize/Plugins/Seo.php
+++ src/Mechanize/Plugins/Seo.php
@@ -4,44 +4,6 @@ namespace Mechanize\Plugins;
 
 class Seo extends Html
 {
-	protected $canonicalUrl = null;
-
-	/**
-	 * Determines if the url has a canonical url
-	 *
-	 * @return mixed The canonical url or bool false
-	 */
-	public function hasCanonicalUrl()
-	{
-		if (!is_null($this->canonicalUrl)) {
-			return $this->canonicalUrl !== null;
-		}
-
-		$canonical = $this->findOne('/html/head/link[@rel="canonical"]');
-
-		if ($canonical->length === 1) {
-			$this->canonicalUrl = $this->getParser()->getAbsoluteUrl($canonical->href);
-
-			return $this->hasCanonicalUrl();
-		}
-
-		return false;
-	}
-
-	/**
-	 * Returns the absolute canonical url to the page
-	 *
-	 * @return mixed The canonical url or bool false
-	 */
-	public function getCanonicalUrl()
-	{
-		if ($this->hasCanonicalUrl()) {
-			return $this->canonicalUrl;
-		}
-
-		return false;
-	}
-
 	/**
 	 * Return dofollow links
 	 *
