c22288f698a173fcba59fc93005ebe0a8d3b55ce
cristiangraz <cristian.graziano@gmail.com>
2012-07-15 18:53:39 -0700

    Getting Schema parser to work

--- src/Mechanize/Plugins/Schema.php
+++ src/Mechanize/Plugins/Schema.php
@@ -4,35 +4,70 @@ namespace Mechanize\Plugins;
 
 class Schema extends AbstractPlugin implements PluginInterface
 {
+	/**
+	 * Class constants
+	 */
+	const ANCESTOR_OF 		= 	'data-ancestor-of';
+	const ANCESTOR_TYPE		=	'data-ancestor-type';
+
+	/**
+	 * Parses for Schemas as defined at http://schema.org
+	 * 
+	 * @return array An array of schemas
+	 */
 	public function getSchemas()
 	{
-		$schemas = $this->find('//*[@itemscope]');
+		// Create a clone of the original parser - getSchema() will remove nodes as they are found
+		$tempParser = clone $this->getParser();
 
-		$elements = array();
-		foreach ($schemas as $i => $schema) {
-			// Find Properties
-			$elements[$i]['type'] = $schema->itemtype;
+		$i=0;
+		while ($this->getSchema($i)) {
+			// Finds the innermost schema and works its way outwards
+			$i++;
+		}
 
-			// Find more elements
-			$properties = $schema->find('//*[@itemprop]');
+		// Revert back to the original Parser object
+		$this->setParser($tempParser);
 
-			if ($properties->length === 0) {
-				continue;
-			}
+		// Cleanup
+		foreach ($this->schemas as &$element) {
+			if (isset($element[self::ANCESTOR_OF])) {
+				$id = $element[self::ANCESTOR_OF];
 
-			foreach ($properties as $property) {
-				if ($property->hasAttribute('content')) {
-					$elements[$i][$property->itemprop] = $property->content;
-				} else {
-					$elements[$i][$property->itemprop] = trim($property->getText());
+				$type = $this->schemas[$id]['type'];
+				if (isset($element[self::ANCESTOR_TYPE])) {
+					$type = $element[self::ANCESTOR_TYPE];
+
+					unset($element[self::ANCESTOR_TYPE]);
 				}
+
+				$element[$type] = $this->schemas[$id];
+
+				unset($element[self::ANCESTOR_OF]);
+				unset($this->schemas[$id]);
 			}
 		}
 
-		echo '<pre>';
-		print_r($elements);
+		// Return everythign the proper order since we went from the innermost node outwards
+		$this->schemas = array_reverse($this->schemas);
 
-		exit;
+		return $this->schemas;
+	}
+
+	/**
+	 * Parses out the Schema type from the itemtype attribute
+	 * 
+	 * @param string $type The value of the itemtype attribute
+	 *
+	 * @return string
+	 */
+	public function getType($type)
+	{
+		if (false !== preg_match('#/([a-z]+)$#i', $type, $matches)) {
+			return strtolower($matches[1]);
+		}
+
+		return $type;
 	}
 
 	/**
@@ -43,5 +78,90 @@ class Schema extends AbstractPlugin implements PluginInterface
 	public function getAlias()
 	{
 		return 'schema';
+	}
+
+	/**
+	 * Parses an individual schema
+	 * 
+	 * @param integer $schemaIndex The current index used for pointers to other schemas
+	 * @return bool
+	 */
+	private function getSchema($schemaIndex = 0)
+	{
+		// Find the innermost schema
+		$schemas = $this->findOne('//*[@itemscope and not(descendant::*[@itemscope])]');
+
+		if ($schemas->length === 0) {
+			return false;
+		}
+
+		$elements = array();
+		foreach ($schemas as $i => $schema) {
+			// Find Properties
+			$elements['type'] = $this->getType($schema->itemtype);
+			$elements['schema'] = $schema->itemtype;
+
+			// If a pointer was previously set, add it to the element
+			if ($schema->hasAttribute(self::ANCESTOR_OF)) {
+				$elements[self::ANCESTOR_OF] = $schema->getAttribute(self::ANCESTOR_OF);
+			}
+
+			if ($schema->hasAttribute(self::ANCESTOR_TYPE)) {
+				$elements[self::ANCESTOR_TYPE] = $schema->getAttribute(self::ANCESTOR_TYPE);
+			}
+
+			// Set a unique id on each schema type for identification purposes
+			$schema->setAttribute('id', 'schema-id-' . $i);
+
+			// See if this Schema extends from a parent and set a pointer on the parent
+			$ancestor = $this->findOne('//*[@id="schema-id-' . $i . '"]/ancestor::*[@itemscope][last()-1]');
+			if ($ancestor->length === 1) {
+				$ancestor->setAttribute(self::ANCESTOR_OF, $schemaIndex);
+
+				if ($elements['type'] === 'person' && ($schema->itemprop === 'author' || false !== strpos($schema->rel, 'author'))) {
+					// This is a Person schema representing an author
+					$ancestor->setAttribute(self::ANCESTOR_TYPE, 'author');
+				}
+			}
+			
+			$properties = $this->find('//*[@itemscope and @id = "schema-id-' . $i . '"]//*[@itemprop]');
+
+			if ($properties->length > 0) {
+				foreach ($properties as $property) {
+
+					// If the property is an ancestor of anything beginning with schema-id- that does not equal schema-id-$i, skip
+					if ($property->hasAttribute('content')) {
+						$elements[$property->itemprop] = $property->content;
+					} else {
+						if ($property->itemprop === 'url' && $property->getTag() === 'a') {
+							$elements[$property->itemprop] = array(
+								'text' => $property->getText(),
+								'href' => $this->getParser()->getAbsoluteUrl($property->href)
+							);
+						} elseif ($property->getTag() === 'img') {
+							$elements[$property->itemprop] = $this->getParser()->getAbsoluteUrl($property->src);
+						} elseif ($property->itemprop === 'image' || $property->itemprop === 'thumbnailUrl') {
+							// Look for divs, etc marked up as an img where an img tag is embedded
+							$img = $property->findOne('img');
+
+							if ($img->length === 1) {
+								$elements[$property->itemprop] = $this->getParser()->getAbsoluteUrl($img->src);
+							}
+						} elseif ($property->itemprop === 'datePublished') {
+							$elements[$property->itemprop] = new \DateTime($property->itemprop);
+						} else {
+							$elements[$property->itemprop] = $property->getText();
+						}
+					}
+				}
+
+				// Remove from the dom
+				$schemas->remove();
+			}
+		}
+
+		$this->schemas[$schemaIndex] = $elements;
+
+		return true;
 	}
 }
